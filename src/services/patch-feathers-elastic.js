a19f1332d2ac67ca9a904c364838dc6089016630
salamynder <rtbtobi@gmail.com>
2018-03-07 13:44:03 +0100

    patch feathers update

--- src/services/patch-feathers-elastic.js
+++ src/services/patch-feathers-elastic.js
@@ -3,4 +3,9 @@ const patch = require('patch-module')
 module.exports =
     patch('./node_modules/feathers-elasticsearch/lib/index.js'
         , undefined
-        , [{find: 'parent,', replace: '[service.parent]: parent,'}])
+        , [
+            {find: 'parent,', replace: '[service.parent]: parent,'},
+            // {find: 'let { query } = filter(params.query, this.paginate);\n', replace: 'let { query } = filter(params.query, this.paginate);\n debugger\n'},
+            {find: 'parent: query[this.parent]', replace: '[data.routing ? "routing" : "parent"]: data.routing ? data.routing : query[this.parent]'}
+            ,{find: 'body: removeProps(data, this.meta, this.id)', replace: 'body: removeProps(data, this.meta, this.id, "routing", "parent")'}
+        ])
